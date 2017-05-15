22c32f655ede7f265be89a36181e90bdd29d5965
Hongwen Dai <hdai3@blueman8.ece.ncsu.edu>
2017-05-15 02:03:14 -0400

    source code with the sound baseline configuration

--- gpgpusim_con_kernel.SoundBaseline/search.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo searching string \"$1\" in current folder
-find . -name "*.cc" -o -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.l" -o -name "*.y" | xargs grep -n "$1"
